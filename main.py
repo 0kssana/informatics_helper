8a38cdb32ada75a5ac900b6b2f2586829f129985
Derbasova Oksana <onderbasova@edu.hse.ru>
2023-11-17 01:36:43 +0600

    added first interface

---
+++ main.py
@@ -0,0 +1,22 @@
+from converteriMain import converter_interface
+from binOperMain import binary_operations_interface
+from tableMain import truth_table_interface
+
+def main_menu():
+    while True:
+        choice = input(
+            "Выберите опцию:\n1. Конвертация систем счисления\n2. Операции с двоичными числами\n3. Таблица истинности\n4. Выход\nВаш выбор: ")
+        if choice == '1':
+            converter_interface()
+        elif choice == '2':
+            binary_operations_interface()
+        elif choice == '3':
+            truth_table_interface()
+        elif choice == '4':
+            break
+        else:
+            print("Некорректный ввод. Пожалуйста, выберите опцию от 1 до 4.")
+
+
+if __name__ == '__main__':
+    main_menu()
